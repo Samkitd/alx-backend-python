596a8142be21592c5ea2287e8808357b46f9d830
Samkitd <samkitd88@gmail.com>
2025-05-17 18:01:58 +0300

    "Yield used"

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -5,27 +5,27 @@ import os
 load_dotenv()
 
 def stream_users_in_batches(batch_size):
-    connection = mysql.connector.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_NAME')
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
-    cursor = connection.cursor(dictionary=True)
+    cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
 
     try:
-        while True:
+        while True:  # Loop 1
             batch = cursor.fetchmany(batch_size)
             if not batch:
                 break
             yield batch
     finally:
         cursor.close()
-        connection.close()
+        conn.close()
 
 def batch_processing(batch_size):
-    for batch in stream_users_in_batches(batch_size):  # loop 1
-        for user in batch:                              # loop 2
+    for batch in stream_users_in_batches(batch_size):  # Loop 2
+        for user in batch:                             # Loop 3
             if user['age'] > 25:
-                yield user  # ✅ yield, not print
+                yield user 
